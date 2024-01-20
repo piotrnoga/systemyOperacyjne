fcf92f265d68bfa3b02e31abc91143d7db3fc58f
piotrnoga <piotrnoga.dev@gmail.com>
2024-01-20 10:06:31 +0100

    Dodano plik kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,19 @@
+def dodawanie(x, y):
+	return x + y
+
+def odejmowanie(x, y):
+	return x - y
+
+def mnozenie(x, y):
+	return x * y
+
+def dzielenie(x, y)
+	if y !=0:
+		return x / y
+	else:
+		return "Nie można dzielić przez zero."
+
+print(dodawanie(25, 17))
+print(odejmowanie(25, 17))
+print(mnozenie(2, 5))
+print(dzielenie(3, 0))
